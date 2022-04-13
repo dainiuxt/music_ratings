d48bec168ccff010d7211a1b0eecadaf4298033a
dainiuxt <dainiuxt@gmail.com>
2022-04-13 21:42:40 +0300

    add __str__ to models

--- music_ratings_api/admin.py
+++ music_ratings_api/admin.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 from .models import Band, Album, Song, AlbumReview, AlbumReviewComment, AlbumReviewLike
 # Register your models here.
 
-admin.site.register(Band)
-admin.site.register(Album)
+class SongInline(admin.TabularInline):
+    model = Song
+    extra = 0
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('band', 'title')
+    inlines = [SongInline]
+
+class AlbumInline(admin.TabularInline):
+    model = Album
+    extra = 0
+
+class BandAdmin(admin.ModelAdmin):
+  inlines = [AlbumInline]
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Song)
 admin.site.register(AlbumReview)
 admin.site.register(AlbumReviewComment)
 admin.site.register(AlbumReviewLike)
+
--- music_ratings_api/models.py
+++ music_ratings_api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-# Create your models here.
-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,15 +7,40 @@ from django.contrib.auth.models import User
 class Band(models.Model):
     title = models.CharField(max_length=150)
 
+    def __str__ (self):
+      return f"{self.title}"
+
 
 class Album(models.Model):
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
     title = models.CharField(max_length=1000)
 
+    def __str__ (self):
+      return f"{self.title}"
+
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField(max_length=1000)
     duration = models.TimeField()
+
+    ORDER_STATUS = (
+        ('o', 'Side one'),
+        ('t', 'Side two'),
+        ('b', 'Bonus track'),
+        ('e', 'Other'),
+        )
+
+    status = models.CharField(
+        max_length=1,
+        choices=ORDER_STATUS,
+        blank=True,
+        default='0',
+        help_text='Status',
+        )
+
+    def __str__ (self):
+      album = self.album.title
+      return f"{self.title} from {self.album}; ({self.duration})"
 
 class AlbumReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,10 +54,17 @@ class AlbumReview(models.Model):
         ]
      )
 
+    def __str__ (self):
+      user = self.user.name
+      return f"{self.ratings} by {self.user}"
+
 class AlbumReviewComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.ForeignKey(AlbumReview, on_delete=models.CASCADE)
     content = models.CharField(max_length=5000)
+
+    def __str__ (self):
+      return f"{self.user}; {self.content}"
 
 class AlbumReviewLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
